ef58fb36fdea1a2ab2c4f09caa70ec3a09b32c90
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-27 10:51:26 +0200

    Hello message to repository

--- src/initialization/Running_Tests.java
+++ src/initialization/Running_Tests.java
@@ -6,6 +6,7 @@ public class Running_Tests {
 		
 		System.out.println("Initializing repository...");
 		System.out.println("Integration in process...");
+		System.out.print("Hello from Thinkpad w520 to repository!");
 
 	}
 
