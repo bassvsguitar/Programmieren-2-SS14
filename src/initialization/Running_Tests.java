84544c87716bea34002b36ebb3448bbf4753742f
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-27 10:56:32 +0200

    Update Running_Tests.java

--- src/initialization/Running_Tests.java
+++ src/initialization/Running_Tests.java
@@ -8,6 +8,8 @@ public class Running_Tests {
 		System.out.println("Integration in process...");
 		System.out.print("Hello from Thinkpad w520 to repository!");
 		System.out.print("Hello from Thinkpad x220t to repository!");
+		System.out.println("Integration on both notebooks successful!");
+		system.out.println("Hello from Programmieren-2-SS14");
 
 	}
 
