cda2837802266c113e2c1951e845e39f40acfc38
bassvsguitar <oliver.zientarski@gmail.com>
2014-06-27 10:54:28 +0200

    Hello message from Thinkpad x220t

--- src/initialization/Running_Tests.java
+++ src/initialization/Running_Tests.java
@@ -7,6 +7,7 @@ public class Running_Tests {
 		System.out.println("Initializing repository...");
 		System.out.println("Integration in process...");
 		System.out.print("Hello from Thinkpad w520 to repository!");
+		System.out.print("Hello from Thinkpad x220t to repository!");
 
 	}
 
